1537c4a68a0aec647013b653b80e8a396fb544bc
BPN\marius <marius.klug@tu-berlin.de>
2018-06-05 16:22:30 +0200

    Added a functionality to plot single subject ERSPs. Added a function to create events based on peaks in an ICA component (like blink or heartbeat).

--- bemobil_plot_ERSPs.m
+++ bemobil_plot_ERSPs.m
@@ -1,4 +1,4 @@
-function plot = bemobil_plot_ERSPs(average_time_frequency_data,  all_subjects_time_frequency_data, STUDY, ALLEEG, scale)
+function [averages_plot, single_subject_plots] = bemobil_plot_ERSPs(average_time_frequency_data,  all_subjects_time_frequency_data, STUDY, ALLEEG, scale)
 
 % find residual variance info for title later
 if ~isfield(STUDY.cluster, 'mean_rv')
@@ -19,7 +19,7 @@ if  ~isempty(average_time_frequency_data.info.experiment_conditions)
     n_subplots = n_subplots+1; % difference plot
     
     for condition = 1:length(average_time_frequency_data.info.experiment_conditions)
-        n_subplots = n_subplots+1;
+        n_subplots = n_subplots+2;
         n_ERSPs = n_ERSPs+1;
         condmax = max(max(abs(average_time_frequency_data.(['condition_' num2str(condition)]).ersp)));
         if condmax > this_scale
@@ -29,7 +29,7 @@ if  ~isempty(average_time_frequency_data.info.experiment_conditions)
 end
 
 % apparently this is the standard way to go, EEGLAB at least does it so:
-if isempty(scale) 
+if isempty(scale)
     this_scale = this_scale/2;
 else
     this_scale = scale;
@@ -52,13 +52,13 @@ end
 
 
 % start the plot
-plot = figure;
+averages_plot = figure;
 
 % plot dipoles if possible
 if do_dipoles
     subplot(n_subplots,1,1);
     std_dipplot(STUDY,ALLEEG,'clusters',average_time_frequency_data.info.cluster,'figure','off');
-    set(plot,'Color','white')
+    set(averages_plot,'Color','white')
 end
 plot_alpha=false;
 % plot average ERSPs
@@ -102,20 +102,20 @@ for ERSP = 1:n_ERSPs+1
             average_time_frequency_data.freqs,...
             data_to_plot_this_condition,...
             [-this_scale this_scale]);%,...
-%             [average_time_frequency_data.times(1):200:average_time_frequency_data.times(end)]);
+        %             [average_time_frequency_data.times(1):200:average_time_frequency_data.times(end)]);
         set(gca,'XMinorTick','on')
         axis xy;
         
-%         xticks(0:200:xlim);
+        %         xticks(0:200:xlim);
         
         % plot vlines of baseline in grey
         vertical_line(average_time_frequency_data.info.baseline_start_end,'white',2);
-% lines =average_time_frequency_data.info.baseline_start_end;
-% yLimits=get(gca,'ylim');             % Row vector
-% Ylimits=repmat(yLimits', 1, length(lines));
-% hold on
-%  handle = gca;
-%  plot(handle,[lines';lines'], Ylimits,'LineWidth',2);
+        % lines =average_time_frequency_data.info.baseline_start_end;
+        % yLimits=get(gca,'ylim');             % Row vector
+        % Ylimits=repmat(yLimits', 1, length(lines));
+        % hold on
+        %  handle = gca;
+        %  plot(handle,[lines';lines'], Ylimits,'LineWidth',2);
         
         if do_vlines
             % plot vlines of timewarp in black
@@ -176,123 +176,93 @@ disp('done')
 
 
 % plot single subject ERSPs
-% disp('Plotting single subject ERSPs...')
-%
-% for subject = 1:length(all_subjects_time_frequency_data)
-%
-%
-%
-%     for ERSP = 1:n_ERSPs+1
-%         try
-%
-%             if ERSP == 1
-%
-%                 % if grand average plot
-%                 fprintf('Plotting grand average data...\n')
-%                 this_condition = average_time_frequency_data.grand_average;
-%
-%             elseif ERSP == n_ERSPs+1
-%
-%                 % last plot is difference plot
-%                 fprintf('Plotting difference data...\n')
-%                 this_condition = average_time_frequency_data.difference;
-%
-%             else
-%
-%                 % if a condition plot
-%                 fprintf('Plotting condition_%d data...\n', ERSP-1)
-%                 this_condition = average_time_frequency_data.(['condition_' num2str(ERSP-1)]);
-%             end
-%
-%             data_to_plot_this_condition = this_condition.ersp;
-%
-%
-%             if ~isempty(this_condition.statistics)
-%                 error('statistics not implemented yet')
-%                 calculated_statistics = true;
-%                 masked_data_to_plot_this_condition = data_to_plot_this_condition.*squeeze(p_values_mask(cluster,plot,:,:));
-%             else
-%                 disp('No statistics data present in this condition. No contour lines for masking will be plotted.')
-%                 calculated_statistics = false;
-%             end
-%
-%             subplot(n_subplots,1,ERSP+1);
-%
-%             imagesclogy(average_time_frequency_data.times,...
-%                 average_time_frequency_data.freqs,...
-%                 data_to_plot_this_condition,...
-%                 [-scale scale]);
-%
-%             axis xy;
-%
-%             if do_vlines
-%                 % plot vlines of timewarp in black
-%                 vline(average_time_frequency_data.info.latencyMeans,'black');
-%             end
-%
-%             % plot vlines of baseline in grey
-%             vline(average_time_frequency_data.info.baseline_start_end,'white');
-%
-%             if isfield(this_condition,'n_epochs') && isfield(this_condition,'n_epochs_means')
-%                 title([strjoin(' ',strsplit(this_condition.condition_title,'_')) ', ' num2str(this_condition.n_epochs) ' epochs, mean: ' num2str(this_condition.n_epochs_means)]);
-%             else
-%                 title(strjoin(' ',strsplit(this_condition.condition_title,'_')));
-%             end
-%             hold on
-%
-%             if calculated_statistics
-%                 contour(times,...
-%                     freqs,...
-%                     logical(masked_data_to_plot_this_condition),...
-%                     1,'linecolor','black');
-%             end
-%
-%             % plot color scaling bar
-%             cbar;
-%
-%         catch
-%             warning('Potato error message: U dun fucked up in plot ERSPs.')
-%         end
-%
-%     end
-%
-%     ax=axes('Units','Normal','Position',[.075 .075 .85 .85],'Visible','off');
-%
-%     set(get(ax,'Title'),'Visible','on')
-%
-%     plot_title = {['Cluster: ' num2str(average_time_frequency_data.info.cluster) ', ' num2str(length(average_time_frequency_data.info.subjects_used))...
-%         ' subjects, ' num2str(length(average_time_frequency_data.info.ICs_used)) ' ICs'] };
-%
-%     if do_dipoles
-%         plot_title{end+1} = ['Mean RV: ' num2str(round(STUDY.cluster(average_time_frequency_data.info.cluster).mean_rv,3)*100) '%'];
-%     end
-%
-%     if calculated_statistics
-%         error('nope')
-%         plot_title{end+1} = ['Significance level : ' num2str(alpha) ];
-%     end
-%
-%     title(plot_title);
-%
-%     disp('done')
-%
-% end
-%
-% disp('...done')
+disp('Plotting single subject ERSPs...')
+
+for subject = 1:length(all_subjects_time_frequency_data)
+    
+    single_subject_plots{subject} = figure;
+    this_time_frequency_data = all_subjects_time_frequency_data(subject);
+    
+    % no difference plots here, but both conditions and the grand average
+    for ERSP = 1:n_ERSPs
+        try
+            
+            if ERSP == 1
+                
+                % if grand average plot
+                %                 fprintf('Plotting grand average data...\n')
+                this_condition = this_time_frequency_data.grand_average;
+                
+                plot_title = ['Subject: ' num2str(subject) ', Trial normalization : '...
+                    num2str(average_time_frequency_data.info.trial_normalization) ', auto epoch cleaning: '...
+                    num2str(average_time_frequency_data.info.do_auto_epoch_cleaning)...
+                    ', ' this_condition.condition_title];
+                
+                
+            else
+                
+                % if a condition plot
+                %                 fprintf('Plotting condition_%d data...\n', ERSP-1)
+                this_condition = this_time_frequency_data.(['condition_' num2str(ERSP-1)]);
+                
+                plot_title = [this_condition.condition_title];
+                
+            end
+            
+            data_to_plot_this_condition = this_condition.ersp;
+            
+            
+            subplot(n_ERSPs,1,ERSP);
+            
+            imagesclogy(this_time_frequency_data.times,...
+                this_time_frequency_data.freqs,...
+                data_to_plot_this_condition,...
+                [-this_scale this_scale]);
+            
+            axis xy;
+            
+            if do_vlines
+                % plot vlines of timewarp in black
+                vline(average_time_frequency_data.info.latencyMeans,'black');
+            end
+            
+            % plot vlines of baseline in grey
+            vline(this_time_frequency_data.info.baseline_start_end,'white');
+            
+            title(plot_title);
+            
+            hold on
+            
+            % plot color scaling bar
+            cbar;
+            
+        catch
+            warning('Potato error message: U dun fucked up in plot ERSPs.')
+        end
+        
+    end
+    
+    ax=axes('Units','Normal','Position',[.075 .075 .85 .85],'Visible','off');
+    
+    set(get(ax,'Title'),'Visible','on')
+    
+end
+
+disp('...done')
 
 
 function [lineHandles] = vertical_line(x_positions, lineType, lineWidth)
-% 
+%
 % vertical_line() - Draws vectical lines into the current plot. Linetype and linewidth can be specified.
 %
 % Usage:
 %   >>  [lineHandles] = vertical_line(x_positions, lineType, lineWidth)
-% 
+%
 % Inputs:
 %   x_positions     - positions on the x-axis where the lines should be drawn
 %   lineType        - same as the ones used in plot()
 %   lineWidth       - width of the line (best to use 1 or 2 at most)
-% 
+%
 % Outputs:
 %   lineHandles     - handle to the drawn line
 %
@@ -315,7 +285,7 @@ axesHandle = gca;
 initial_ishold = ishold(axesHandle);
 hold(axesHandle, 'on');
 
-yLimits=get(axesHandle,'ylim');            
+yLimits=get(axesHandle,'ylim');
 Ylimits=repmat(yLimits', 1, length(x_positions));
 
 if( isempty(lineType) )
