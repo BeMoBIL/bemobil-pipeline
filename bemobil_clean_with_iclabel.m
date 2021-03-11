7a2ba2054ade20423ca594101205c2f314509147
sjeung <seinjeung@gmail.com>
2021-03-11 15:26:14 +0100

    dipole .fig suffix added

--- bemobil_clean_with_iclabel.m
+++ bemobil_clean_with_iclabel.m
@@ -151,7 +151,7 @@ if save_file_on_disk
     
     % save dipole fig
     print(gcf,fullfile(out_filepath,[out_filename '_brain_dipoles']),'-dpng')
-    savefig(fullfile(out_filepath,[out_filename '_brain_dipoles']))
+    savefig(fullfile(out_filepath,[out_filename '_brain_dipoles.fig']))
     close
     print(fig1,fullfile(out_filepath,[out_filename '_ICs_kept.png']),'-dpng')
     close
