327ae083d2b9f00368334891c59b3f58a0482ffc
lukasgehrke <lukas.gehrke@tu-berlin.de>
2021-07-27 16:25:22 +0200

    Improved name check of rigid body quaternion dimensions

--- import/bemobil_bids_motionconvert.m
+++ import/bemobil_bids_motionconvert.m
@@ -44,14 +44,14 @@ for iM = 1:numel(motionIn)
         
         % check the lines below for quaternion channel names
         for qi = 1:4
-            quaternionIndices(qi) = find(contains(labelsPre, objects{ni}) & contains(labelsPre, ['_quat_' quaternionComponents{qi}]));
+            quaternionIndices(qi) = find(contains(labelsPre, objects{ni}) & contains(labelsPre, [objects{ni} '_quat_' quaternionComponents{qi}], 'IgnoreCase', true));
         end
         
         cartIndices = NaN(1,3);
         
         % check the lines below for position channel names
         for ci = 1:3
-            cartIndices(ci) = find(contains(labelsPre, objects{ni}) & contains(labelsPre, ['_rigid_' cartCoordinates{ci}]));
+            cartIndices(ci) = find(contains(labelsPre, objects{ni}) & contains(labelsPre, [objects{ni} '_' cartCoordinates{ci}], 'IgnoreCase', true));
         end
         
         % convert from quaternions to euler angles
